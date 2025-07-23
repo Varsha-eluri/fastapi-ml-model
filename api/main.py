dca23e0eb9583db622af158a8061a7c2a1965ff2
Varsha-eluri <varshaeluri123@gmail.com>
2025-07-23 15:05:51 +0530

    Add files via upload

---
+++ api/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI 
+from pydantic import 
+import joblib 
+import os
+
+app=FastAPI()
+
+model_path=os.path.join(os.path.dirname(__file__),"model.pkl")
+model=joblib.load(model_path)
+class InputData(BaseModel):
+    features:list[float]
+@app.post("/predict")
+def predict(data:InputData):
+    prediction=model.predict([data.features])
+    return {"predictions":int(prediction[0])}
